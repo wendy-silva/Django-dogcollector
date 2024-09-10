197858fedcb87b20c8f427892c80e63b4eb9c202
wendy-silva <silvawendy0131@gmail.com>
2024-09-10 14:40:19 -0400

    model/serializers

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,9 @@
-# import Home view from the views file
-from .views import Home
+from django.urls import path
+from .views import Home, DogList, DogDetail # additional imports
 
 urlpatterns = [
   path('', Home.as_view(), name='home'),
+  # new routes below 
+  path('dogs/', DogList.as_view(), name='dog-list'),
+  path('dogs/<int:id>/', DogDetail.as_view(), name='dog-detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
+from .models import Dog
+from .serializers import DogSerializer
 
 # Define the home view
 class Home(APIView):
   def get(self, request):
-    content = {'message': 'Welcome to the cat-collector api home route!'}
+    content = {'message': 'Welcome to the dog-collector api home route!'}
     return Response(content)
+  
+class DogList(generics.ListCreateAPIView):
+  queryset = Dog.objects.all()
+  serializer_class = DogSerializer
+
+class DogDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Dog.objects.all()
+  serializer_class = DogSerializer
+  lookup_field = 'id'
