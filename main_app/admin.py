2c3cfb8a78b275b3fedd3b62fffa493a7c65e236
wendy-silva <silvawendy0131@gmail.com>
2024-09-12 11:17:42 -0400

    django-rep

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 # import your models here
-from .models import Dog, Feeding
+from .models import Dog, Feeding, Toy
 
 # Register your models here
 admin.site.register(Dog)
 admin.site.register(Feeding)
+admin.site.register(Toy)
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, DogList, DogDetail, FeedingListCreate, FeedingDetail 
+from .views import Home, DogList, DogDetail, FeedingListCreate, FeedingDetail, ToyList, ToyDetail
 
 urlpatterns = [
   path('', Home.as_view(), name='home'),
@@ -8,4 +8,6 @@ urlpatterns = [
   path('dogs/<int:id>/', DogDetail.as_view(), name='dog-detail'),
   path('dogs/<int:dog_id>/feedings/', FeedingListCreate.as_view(), name='feeding-list-create'),
 	path('dogs/<int:dog_id>/feedings/<int:id>/', FeedingDetail.as_view(), name='feeding-detail'),
+  path('toys/', ToyList.as_view(), name='toy-list'),
+  path('toys/<int:id>/', ToyDetail.as_view(), name='toy-detail'),
 ]
