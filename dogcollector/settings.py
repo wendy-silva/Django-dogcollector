96556e516533014053b8fffb1f34ebb0450b2ffb
wendy-silva <silvawendy0131@gmail.com>
2024-09-13 10:03:04 -0400

    comment out secret key

--- dogcollector/settings.py
+++ dogcollector/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6dw+5p5mz5lonzwofbo2uy-%ue!pu%2eb_r-j13*6v0$)z9&55'
+# SECRET_KEY = 'django-insecure-6dw+5p5mz5lonzwofbo2uy-%ue!pu%2eb_r-j13*6v0$)z9&55'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
